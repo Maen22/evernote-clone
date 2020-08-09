a421508d472359e8a4b6993a0a9ea4ec25545c5d
maenibreigheth <m3n.ibregeth@gmail.com>
2020-08-09 15:10:20 +0300

    initail layout and functionality + firebase config

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import firebase from "firebase/app";
+import Sidebar from "./sidebar/sidebar";
+import Editor from "./editor/editor";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <Sidebar
+          selectedNoteIndex={this.state.selectedNoteIndex}
+          notes={this.state.notes}
+        />
+        <Editor />
+      </div>
+    );
+  }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection("notes")
+      .onSnapshot((serverUpdate) => {
+        const notes = serverUpdate.docs.map((_doc) => {
+          const data = _doc.data();
+          data["id"] = _doc.id;
+          return data;
+        });
+        console.log(notes);
+        this.setState({ notes });
+      });
+  };
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app'
+import 'firebase/firestore';
+
+const firebaseConfig = firebase.initializeApp({
+    // Your web app's Firebase configuration
+  
+    apiKey: "AIzaSyBYi3SV-pd0R5dP5TkV1Fpq6kMNq_AuxuM",
+    authDomain: "evernote-clone-c40a0.firebaseapp.com",
+    databaseURL: "https://evernote-clone-c40a0.firebaseio.com",
+    projectId: "evernote-clone-c40a0",
+    storageBucket: "evernote-clone-c40a0.appspot.com",
+    messagingSenderId: "777794901902",
+    appId: "1:777794901902:web:e50b9e97aa6b43c5e236df",
+    measurementId: "G-7RSH0NSTNF"
+  });
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('evernote-container')
 );
 
 // If you want your app to work offline and load faster, you can change
